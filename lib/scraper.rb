d554fe2d0551ed5c92bf3d8da5a3ada4355be15b
dslivken <slivken@gmail.com>
2017-01-15 09:38:24 -0500

    corrected branch issues again, all brand and print functions back together

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,9 +19,29 @@ class Scraper
       		link=p.css('a')[0]['href']
       		if link[0]=="/"
         		link = "http://www.globusjourneys.com#{link}"
+        		brand = "Globus"
+        	elsif link.include?("avalon")
+				brand="Avalon"
+			elsif link.include?("cosmos")
+				brand="Cosmos"
+			else
+				brand="Monograms"
       		end
-      		 scrape << {:name => p.css('h3').text, :link => link, :descrip =>p.css('h4').text, :price =>p.css('p.listing-price').text.split("\r\n")[1].strip}
+      		 scrape << {:name => p.css('h3').text, :brand => brand, :link => link, :descrip =>p.css('h4').text, :price =>p.css('p.listing-price').text.split("\r\n")[1].strip}
       	end
       	scrape
+	end
+
+	def print_search
+	    puts "- - - - - - - - - - - - - - -"
+	    @tours.each_with_index do |t, i|
+	    	puts "TOUR # #{i+1}"
+	    	t.each do |key, value|
+	        	if key != :link
+	          		puts "#{key}: #{value}"
+	        	end
+	      	end
+	      	puts "- - - - - - - - - - - - - - -"
+	    end
 	end
 end
