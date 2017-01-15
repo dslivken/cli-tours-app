33f838786b63c59ee95fa73d061d0883bef961c0
dslivken <slivken@gmail.com>
2017-01-15 09:27:49 -0500

    added brand identifier

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 class Scraper
 
-	attr_accessor :tours, :name, :link, :descrip, :price
+	attr_accessor :tours, :name, :link, :descrip, :price, :brand
 
 	BASEURL= 'http://www.globusjourneys.com/Travel-Tour-Packages/Reservations/Vacation-Search/?keyword='
 
@@ -19,8 +19,15 @@ class Scraper
       		link=p.css('a')[0]['href']
       		if link[0]=="/"
         		link = "http://www.globusjourneys.com#{link}"
+        		brand = "Globus"
+		    elsif link.include?("avalon")
+		        brand="Avalon"
+		    elsif link.include?("cosmos")
+		        brand="Cosmos"
+		    else
+		        brand="Monograms"        	
       		end
-      		 scrape << {:name => p.css('h3').text, :link => link, :descrip =>p.css('h4').text, :price =>p.css('p.listing-price').text.split("\r\n")[1].strip}
+      		 scrape << {:name => p.css('h3').text, :brand => brand, :link => link, :descrip =>p.css('h4').text, :price =>p.css('p.listing-price').text.split("\r\n")[1].strip}
       	end
       	scrape
 	end
