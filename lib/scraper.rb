2de1a65fec9626f14a7029391fbba0a4292d5e7e
dslivken <slivken@gmail.com>
2017-01-15 09:30:35 -0500

    reconciling branches

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,6 @@
 class Scraper
 
-	attr_accessor :tours, :name, :link, :descrip, :price, :brand
+	attr_accessor :tours, :name, :link, :descrip, :price
 
 	BASEURL= 'http://www.globusjourneys.com/Travel-Tour-Packages/Reservations/Vacation-Search/?keyword='
 
@@ -19,15 +19,8 @@ class Scraper
       		link=p.css('a')[0]['href']
       		if link[0]=="/"
         		link = "http://www.globusjourneys.com#{link}"
-        		brand = "Globus"
-		    elsif link.include?("avalon")
-		        brand="Avalon"
-		    elsif link.include?("cosmos")
-		        brand="Cosmos"
-		    else
-		        brand="Monograms"        	
       		end
-      		 scrape << {:name => p.css('h3').text, :brand => brand, :link => link, :descrip =>p.css('h4').text, :price =>p.css('p.listing-price').text.split("\r\n")[1].strip}
+      		 scrape << {:name => p.css('h3').text, :link => link, :descrip =>p.css('h4').text, :price =>p.css('p.listing-price').text.split("\r\n")[1].strip}
       	end
       	scrape
 	end
