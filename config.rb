bfcbb75b0282c017e2b5b910843f546feef095be
dslivken <slivken@gmail.com>
2017-01-15 20:34:46 -0500

    started creating methods for cli file

--- config.rb
+++ config.rb
@@ -3,4 +3,4 @@ require 'open-uri'
 require 'pry'
 require_relative "./lib/scraper.rb"
 require_relative "./lib/tour.rb"
-require_relative "./lib/globusSearch.rb"
+require_relative "./lib/globus_search/cli.rb"
--- lib/globus_search/cli.rb
+++ lib/globus_search/cli.rb
@@ -1,17 +1,24 @@
-class GlobusSearch
+class CLI
 
 	def call
+		#welcome
 		puts "Welcome to the Globus search gem"
+		#get_location
 		puts "Enter the location you want to see tours for:"
 		input = gets.strip
 		while input != "exit" do
-			if Scraper.tours.has_key?(input) == false
-				Scraper.new(input)
-			end
+			#check new_search?
+			#if Scraper.tours.has_key?(input) == false
+			#	Scraper.new(input)
+			#end
+			new_search?(input)
 			if Scraper.tours[input].size == 0
+				#search_not_found
 				puts "Sorry, nothing found!!"
 			elsif
+				#print_location_results
 				Scraper.print_search(input)
+				#get_tour
 				puts "Enter the number of the #{input} tour you want details for (or 'exit':)"
 				tour=gets.strip
 				while tour != "exit" do
@@ -31,4 +38,11 @@ class GlobusSearch
 
 		puts "THANK YOU!"
 	end
+
+	def new_search?(input)
+		if Scraper.tours.has_key?(input) == false
+			Scraper.new(input)
+		end
+	end
+
 end
